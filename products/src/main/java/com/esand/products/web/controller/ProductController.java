751601283fcd9fa31dbe2944839e7df1b180dc96
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-08-03 18:42:22 -0300

    remove: Remoção da anotação @Override desnecessária

--- products/src/main/java/com/esand/products/web/controller/ProductController.java
+++ products/src/main/java/com/esand/products/web/controller/ProductController.java
@@ -24,74 +24,62 @@ public class ProductController implements SpringDoc {
 
     private final ProductService productService;
 
-    @Override
     @PostMapping
     public ResponseEntity<ProductResponseDto> create(@RequestBody @Valid ProductCreateDto dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(dto));
     }
 
-    @Override
     @GetMapping
     public ResponseEntity<PageableDto> findAll(@PageableDefault(size = 10) Pageable pageable) {
         return ResponseEntity.ok(productService.findAll(pageable));
     }
 
-    @Override
     @GetMapping("/title/{title}")
     public ResponseEntity<ProductResponseDto> findByTitle(@PathVariable String title) {
         return ResponseEntity.ok(productService.findByTitle(title));
     }
 
-    @Override
     @GetMapping("/supplier/{supplier}")
     public ResponseEntity<PageableDto> findBySupplier(@PageableDefault(size = 10) Pageable pageable, @PathVariable String supplier) {
         return ResponseEntity.ok(productService.findBySupplier(pageable, supplier));
     }
 
-    @Override
     @GetMapping("/category/{category}")
     public ResponseEntity<PageableDto> findByCategory(@PageableDefault(size = 10) Pageable pageable, @PathVariable @Valid String category) {
         return ResponseEntity.ok(productService.findByCategory(pageable, category));
     }
 
-    @Override
     @GetMapping("/sku/{sku}")
     public ResponseEntity<ProductResponseDto> findBySku(@PathVariable String sku) {
         return ResponseEntity.ok(productService.findBySku(sku));
     }
 
-    @Override
     @GetMapping("/actived")
     public ResponseEntity<PageableDto> findAllActived(@PageableDefault(size = 10) Pageable pageable) {
         return ResponseEntity.ok(productService.findAllActived(pageable));
     }
 
-    @Override
     @GetMapping("/disabled")
     public ResponseEntity<PageableDto> findAllDisabled(@PageableDefault(size = 10) Pageable pageable) {
         return ResponseEntity.ok(productService.findAllDisabled(pageable));
     }
 
-    @Override
     @PutMapping("/edit/{sku}")
     public ResponseEntity<Void> update(@PathVariable String sku, @RequestBody @Valid ProductUpdateDto dto) {
         productService.update(sku, dto);
         return ResponseEntity.noContent().build();
     }
 
-    @Override
     @PatchMapping("/status/{sku}")
     public ResponseEntity<String> toggleStatus(@PathVariable String sku) {
         return ResponseEntity.ok("updated status for: " + productService.alter(sku));
     }
 
-    @Override
     @PatchMapping("/sku/{sku}/add/{quantity}")
     public ResponseEntity<String> increaseQuantity(@PathVariable String sku, @PathVariable Integer quantity) {
         return ResponseEntity.ok("updated quantity for: " + productService.add(sku, quantity));
     }
 
-    @Override
     @PatchMapping("/sku/{sku}/sub/{quantity}")
     public ResponseEntity<String> decreaseQuantity(@PathVariable String sku, @PathVariable Integer quantity) {
         return ResponseEntity.ok("updated quantity for: " + productService.sub(sku, quantity));
