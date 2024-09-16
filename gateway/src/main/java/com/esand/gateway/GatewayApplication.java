51ac0b8db6c818fe44783a0b878293081c1e505d
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-09-16 16:59:03 -0300

    feat: Adição de centralização das requisições do actuator no gateway

--- gateway/src/main/java/com/esand/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/esand/gateway/GatewayApplication.java
@@ -23,6 +23,34 @@ public class GatewayApplication {
 				.route(r -> r.path("/api/v1/customers/**").uri("lb://customers"))
 				.route(r -> r.path("/api/v1/orders/**").uri("lb://orders"))
 				.route(r -> r.path("/api/v1/deliveries/**").uri("lb://delivery"))
+
+				.route(r -> r.path("/actuator/products")
+						.filters(f -> f.rewritePath("/actuator/products", "/actuator"))
+						.uri("lb://products"))
+				.route(r -> r.path("/actuator/products/**")
+						.filters(f -> f.rewritePath("/actuator/products/(?<segment>.*)", "/actuator/${segment}"))
+						.uri("lb://products"))
+
+				.route(r -> r.path("/actuator/customers")
+						.filters(f -> f.rewritePath("/actuator/customers", "/actuator"))
+						.uri("lb://customers"))
+				.route(r -> r.path("/actuator/customers/**")
+						.filters(f -> f.rewritePath("/actuator/customers/(?<segment>.*)", "/actuator/${segment}"))
+						.uri("lb://customers"))
+
+				.route(r -> r.path("/actuator/orders")
+						.filters(f -> f.rewritePath("/actuator/orders", "/actuator"))
+						.uri("lb://orders"))
+				.route(r -> r.path("/actuator/orders/**")
+						.filters(f -> f.rewritePath("/actuator/orders/(?<segment>.*)", "/actuator/${segment}"))
+						.uri("lb://orders"))
+
+				.route(r -> r.path("/actuator/deliveries")
+						.filters(f -> f.rewritePath("/actuator/deliveries", "/actuator"))
+						.uri("lb://delivery"))
+				.route(r -> r.path("/actuator/deliveries/**")
+						.filters(f -> f.rewritePath("/actuator/deliveries/(?<segment>.*)", "/actuator/${segment}"))
+						.uri("lb://delivery"))
 				.build();
 	}
 }
