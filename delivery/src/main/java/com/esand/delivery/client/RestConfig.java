bcf4c13243694938d28ca9e8ec9fa1a78f000abc
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-09-16 11:49:58 -0300

    remove: remoção de código não utilizado

--- delivery/src/main/java/com/esand/delivery/client/RestConfig.java
+++ delivery/src/main/java/com/esand/delivery/client/RestConfig.java
@@ -2,11 +2,9 @@ package com.esand.delivery.client;
 
 import com.esand.delivery.client.products.ProductClient;
 import lombok.RequiredArgsConstructor;
-import org.keycloak.admin.client.Keycloak;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -16,8 +14,6 @@ import java.util.function.Consumer;
 @Configuration
 @RequiredArgsConstructor
 public class RestConfig {
-
-    private final Keycloak keycloak;
 
     private final KeycloakTokenInterceptor tokenInterceptor;
 
