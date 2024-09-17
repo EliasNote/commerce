7c6db96dea699e363538afe06d6c05840c640667
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-09-17 17:20:28 -0300

    feat: Adição da url de acesso ao actuator e do docs-api do swagger

--- gateway/src/main/java/com/esand/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/esand/gateway/SecurityConfig.java
@@ -13,8 +13,19 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
-            "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+    private final String[] freeResourceUrls = {
+            "/products/docs-ui/**", "/products/docs-api/**",
+            "/customers/docs-ui/**", "/customers/docs-api/**",
+            "/orders/docs-ui/**", "/orders/docs-api/**",
+            "/deliveries/docs-ui/**", "/deliveries/docs-api/**"
+    };
+
+    private final String[] adminResourceUrls = {
+            "/products/actuator/**", "/products/actuator",
+            "/customers/actuator/**", "/customers/actuator",
+            "/orders/actuator/**", "/orders/actuator",
+            "/deliveries/actuator/**", "/deliveries/actuator"
+    };
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -22,11 +33,13 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(authorize -> authorize
                         .pathMatchers(freeResourceUrls).permitAll()
+                        .pathMatchers(HttpMethod.GET, adminResourceUrls).hasRole("ADMIN")
                         .anyExchange().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults()))
                 .build();
     }
+
 
     @Bean
     public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
