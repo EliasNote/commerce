cf691e6856a5f3015f6907c939fb7c1499082975
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-10-01 22:00:02 -0300

    feat: Criação da entidade para buscar os dados no banco de dados

---
+++ delivery/src/main/java/com/esand/delivery/entity/KeycloakAccess.java
@@ -0,0 +1,32 @@
+package com.esand.delivery.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "keycloak_access")
+public class KeycloakAccess {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String realm;
+
+    @Column(name = "client_id")
+    private String clientId;
+
+    @Column(name = "client_secret")
+    private String clientSecret;
+
+    private String username;
+
+    private String password;
+}
